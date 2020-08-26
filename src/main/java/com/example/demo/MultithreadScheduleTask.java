3622d0b315a6d7353f822cd4edfbb3e11a2dc452
chenjianeng <chenjianeng@jia-fu.cn>
2020-08-26 18:40:27 +0800

    fix

--- src/main/java/com/example/demo/MultithreadScheduleTask.java
+++ src/main/java/com/example/demo/MultithreadScheduleTask.java
@@ -18,7 +18,7 @@ import java.time.LocalDateTime;
 public class MultithreadScheduleTask {
 
     @Async
-    @Scheduled(fixedDelay = 1000)  //间隔1秒
+    @Scheduled(fixedDelay = 1000000000)  //间隔1秒
     public void first() throws InterruptedException {
         System.out.println("第一个定时任务开始 : " + LocalDateTime.now().toLocalTime() + "\r\n线程 : " + Thread.currentThread().getName());
         System.out.println();
@@ -26,7 +26,7 @@ public class MultithreadScheduleTask {
     }
 
     @Async
-    @Scheduled(fixedDelay = 2000)
+    @Scheduled(cron = "0 0/1 14,15 * * ?")
     public void second() {
         System.out.println("第二个定时任务开始 : " + LocalDateTime.now().toLocalTime() + "\r\n线程 : " + Thread.currentThread().getName());
         System.out.println();
---
+++ src/main/java/com/example/demo/SyncTimUserTask.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author: chenjianeng
+ * @Date：2020/8/25 13:16
+ */
+@Component
+public class SyncTimUserTask{
+
+}
